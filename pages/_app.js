d42287cb62c8ac03190efa98c220f67ced553b9c
justremember <nothingpersonal.qwerty@gmail.com>
2022-12-02 16:18:54 +0800

    bootstrap install & setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, [])
+
   return <Component {...pageProps} />
 }
 
