94af53be3728d416a62505d3a96f56382567a716
justremember <nothingpersonal.qwerty@gmail.com>
2022-12-02 17:17:11 +0800

    add absolute import path (for example import '@/styles/globals.css')

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css';
+import '@/styles/globals.css';
 import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
