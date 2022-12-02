bde52edf0cb8700d3be1d09bf0235b7b4e18a3b3
justremember <nothingpersonal.qwerty@gmail.com>
2022-12-02 17:36:56 +0800

    add <Head> and title to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css';
 import { useEffect } from 'react';
+import Head from 'next/head';
+
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     require('bootstrap/dist/js/bootstrap.bundle.min.js');
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Shogi Clock</title>
+        <meta name="description" content="Organize Shogi Swiss tournaments" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
