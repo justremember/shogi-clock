1e7d8e13365b7df0365471071abe1751066d242e
justremember <nothingpersonal.qwerty@gmail.com>
2022-12-15 15:30:09 +0800

    add working reset button

--- components/ClockBody.js
+++ components/ClockBody.js
@@ -1,10 +1,25 @@
+import { useContext } from 'react';
+import { ClockContext } from '@/hooks/clockState';
 import ClockButton from '@/components/ClockButton';
 
 export default function ClockBody() {
+  const { clockState, clockDispatch } = useContext(ClockContext);
+
   return (
     <div>
-      <ClockButton id={0} />
-      <ClockButton id={1} />
+      <div>
+        <ClockButton id={0} />
+        <ClockButton id={1} />
+      </div>
+      <div>
+        <button onClick={() => {
+          if (confirm('Are you sure?')) {
+            clockDispatch({type: 'reset'})
+          }
+        }}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
