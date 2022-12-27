2eb029be7e84721d53fc8952989fc0a04ef5cc18
justremember <nothingpersonal.qwerty@gmail.com>
2022-12-27 15:48:51 +0800

    make tutorial look better with colors & picture. add @/public to jsconfig.json.

--- components/ClockTutorial.js
+++ components/ClockTutorial.js
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+import qwerty from '@/public/images/qwerty.png';
+
+
 export default function ClockTutorial() {
   return (
     <div id='tutorial' className='mt-3'>
@@ -6,34 +10,39 @@ export default function ClockTutorial() {
         Shogi Clock is a chess clock meant to be used for shogi games.
       </p>
       <h3>Keyboard controls</h3>
-      <p>
-        The left clock can be triggered by:
+      <div>
+        The <span style={{color: '#cf2525'}}>left clock</span> can be triggered by:
         <ul>
-          <li>
-            the left arrow key
-          </li>
           <li>
             the keys T, G, and B
           </li>
           <li>
             the alphabetical keys to the left of those three
           </li>
-        </ul>
-      </p>
-      <p>
-        The right clock can be triggered by:
-        <ul>
           <li>
-            the right arrow key
+            the left arrow key
           </li>
+        </ul>
+      </div>
+      <div>
+        The <span style={{color: '#0071cf'}}>right clock</span> can be triggered by:
+        <ul>
           <li>
             the keys Y, H, and N
           </li>
           <li>
-            the alphabetical keys to the right of those three
+            the alphabet keys & symbol keys to the right of those three
+          </li>
+          <li>
+            the right arrow key
           </li>
         </ul>
-      </p>
+      </div>
+      <Image
+        src={qwerty}
+        alt='Keyboard image showing the hotkeys'
+        width={460}
+      />
     </div>
   );
 }
