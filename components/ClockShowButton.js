0311413369fb02dcd11be7ffd66e05ccffda1722
justremember <nothingpersonal.qwerty@gmail.com>
2022-12-30 13:32:57 +0800

    html & css tweaks

--- components/ClockShowButton.js
+++ components/ClockShowButton.js
@@ -1,33 +1,9 @@
-// import useLongPress from '@/hooks/useLongPress';
-// There seems to be a bug with useLongPress. Will not use for now.
-
-function ShowButton({ showSettings, setShowSettings }) {
-  /*
-   const showSettingsLongPress = useLongPress(() => {
-    setShowSettings(true);
-  }, 600);
-  return (
-    <button id='show-settings-button' className='btn btn-outline-primary' {...showSettingsLongPress} >
-      Long press to show settings
-    </button>
-  )
-  */
-  return (
-    <button id='show-settings-button' className='btn btn-outline-primary' onClick={() => setShowSettings(true)} >
-      Show settings
-    </button>
-  )
-}
-
 export default function ClockShowButton({ showSettings, setShowSettings }) {
   return (
     <div>
-      {showSettings ?
-          <button id='show-settings-button' className='btn btn-outline-primary' onClick={() => setShowSettings(false)}>
-            Hide settings
-          </button>
-        : <ShowButton {...{ showSettings, setShowSettings}} />
-      }
+      <button id='show-settings-button' className='btn btn-outline-primary' onClick={() => setShowSettings(s => !s)}>
+        {`${ showSettings ? 'Hide' : 'Show'} settings and tutorial`}
+      </button>
     </div>
   )
 }
